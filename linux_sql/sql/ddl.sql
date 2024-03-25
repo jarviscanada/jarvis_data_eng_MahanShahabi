3bd4c8893eea8e91d6e2feeddd8e872c3eae2c9e
Mahan Shahabi <shahabimahan@gmail.com>
2024-03-25 20:03:02 +0000

    more spacing fix

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -2,17 +2,17 @@
 
 CREATE TABLE IF NOT EXISTS PUBLIC.host_info
   (
-    id               SERIAL NOT NULL,
-    hostname         VARCHAR NOT NULL,
-    cpu_number       INT2 NOT NULL,
-    cpu_architecture VARCHAR NOT NULL,
-    cpu_model        VARCHAR NOT NULL,
-    cpu_mhz          FLOAT8 NOT NULL,
-    l2_cache         INT4 NOT NULL,
-    "timestamp"      TIMESTAMP NULL,
-    total_mem        INT4 NULL,
-    CONSTRAINT host_info_pk PRIMARY KEY (id),
-    CONSTRAINT host_info_un UNIQUE (hostname)
+     id               SERIAL NOT NULL,
+     hostname         VARCHAR NOT NULL,
+     cpu_number       INT2 NOT NULL,
+     cpu_architecture VARCHAR NOT NULL,
+     cpu_model        VARCHAR NOT NULL,
+     cpu_mhz          FLOAT8 NOT NULL,
+     l2_cache         INT4 NOT NULL,
+     "timestamp"      TIMESTAMP NULL,
+     total_mem        INT4 NULL,
+     CONSTRAINT host_info_pk PRIMARY KEY (id),
+     CONSTRAINT host_info_un UNIQUE (hostname)
   );
 
 CREATE TABLE IF NOT EXISTS PUBLIC.host_usage
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS PUBLIC.host_usage
     disk_io        INT4 NOT NULL,
     disk_available INT4 NOT NULL,
     CONSTRAINT host_usage_host_info_fk FOREIGN KEY (host_id) REFERENCES
-    host_info(id)
+        host_info(id)
 );
 
 
