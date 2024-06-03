8cde6c31f44355ac75e5f698acdf52d928567735
Mahan Shahabi <shahabimahan@gmail.com>
2024-06-03 18:07:39 -0400

    controller update

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/StockQuoteController.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/StockQuoteController.java
@@ -21,10 +21,13 @@ public class StockQuoteController {
      */
     public void initClient() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Would you like to buy or sell a stock? (buy/sell)");
+        System.out.println("Select the number of what you would like to do?");
+        System.out.println("1. buy");
+        System.out.println("2. sell");
+        System.out.println("3. view stock");
         String option = sc.nextLine();
 
-        if (option.equals("buy")) {
+        if (option.equals("1")) {
             logger.info("user inputted 'buy'"); // trace log
             System.out.println("Write the symbol of the stock you want to buy");
             String ticker = sc.nextLine();
@@ -33,11 +36,26 @@ public class StockQuoteController {
             Optional<Quote> quote = quoteService.fetchQuoteDataFromAPI(ticker);
             double price = quote.get().getPrice() * numOfShares;
             positionService.buy(ticker, numOfShares, price);
-        } else if (option.equals("sell")) {
+        } else if (option.equals("2")) {
             logger.info("user inputted 'sell'"); // trace log
             System.out.println("Write the symbol of the stock you want to buy");
             String ticker = sc.nextLine();
             positionService.sell(ticker);
+        } else if (option.equals("3")) {
+            logger.info("user inputted 'view stock'"); // trace log
+            System.out.println("Write the symbol of the stock you want to view");
+            String ticker = sc.nextLine();
+            Quote stock = quoteService.fetchQuoteDataFromAPI(ticker).get();
+            System.out.println("Symbol: " + stock.getTicker());
+            System.out.println("Open: " + stock.getOpen());
+            System.out.println("High: " + stock.getHigh());
+            System.out.println("Low: " + stock.getLow());
+            System.out.println("Price: " + stock.getPrice());
+            System.out.println("Volume: " + stock.getVolume());
+            System.out.println("Latest Trading Day: " + stock.getLatestTradingDay());
+            System.out.println("Previous Close: " + stock.getPreviousClose());
+            System.out.println("Change: " + stock.getChange());
+            System.out.println("Change Percent: " + stock.getChangePercent());
         } else {
             logger.error("user inputted invalid entry: {}", sc.nextLine()); // error log
             System.out.println("Invalid entry");
